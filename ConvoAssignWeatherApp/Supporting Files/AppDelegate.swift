8fac4805a4136da31dd823de56c684b1417ba938
umerdeveloper <umar_s66@outlook.com>
2020-05-22 15:50:14 +0500

    naviController setup

--- ConvoAssignWeatherApp/Supporting Files/AppDelegate.swift
+++ ConvoAssignWeatherApp/Supporting Files/AppDelegate.swift
@@ -15,9 +15,6 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     var window: UIWindow?
     
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        window = UIWindow()
-        window?.rootViewController = WeatherVC()
-        window?.makeKeyAndVisible()
         return true
     }
 
