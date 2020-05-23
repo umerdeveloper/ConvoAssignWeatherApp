74758abe2e40c8ea0ead39b59e9dd651a2a9fb9e
umerdeveloper <umar_s66@outlook.com>
2020-05-23 06:32:34 +0500

    Codables Setup

--- ConvoAssignWeatherApp/Controller/WeatherVC.swift
+++ ConvoAssignWeatherApp/Controller/WeatherVC.swift
@@ -11,11 +11,16 @@ import CoreLocation
 
 class WeatherVC: UITableViewController {
     
-    let weatherCellID: String = "weahterCell"
+    let weatherCellID: String   = "weahterCell"
+    let hostURL: String         = "api.openweathermap.org"
+    private let apiKey: String  = "e4bf45ce4eb3ab6e86e8ba2ccede2e4f"
+    let locationManager         = CLLocationManager()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         registerTabelViewCell()
+        updateUserLocation()
+        
     }
     
     // MARK: - TableView DataSource
@@ -26,7 +31,7 @@ class WeatherVC: UITableViewController {
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let weatherCell = tableView.dequeueReusableCell(withIdentifier: weatherCellID, for: indexPath) as! WeatherCell
         
-        weatherCell.tempLabel.text = "\(22)°"
+        weatherCell.tempLabel.text = "\(22)℃"
         weatherCell.weatherStatusLabel.text = "Overcast Clouds"
         return weatherCell
     }
@@ -37,10 +42,50 @@ class WeatherVC: UITableViewController {
         return CGFloat(80)
     }
     
+    // MARK:- Networking
+    fileprivate func fetchWeatherData(with hostURL: String, latitude: String, longitude: String, apiKey: String) {
+        
+        var urlComponents        = URLComponents()
+        urlComponents.scheme     = "https"
+        urlComponents.host       = hostURL
+        urlComponents.path       = "/data/2.5/forecast"
+        urlComponents.queryItems = [
+            URLQueryItem(name: "lat", value: latitude),
+            URLQueryItem(name: "lon", value: longitude),
+            URLQueryItem(name: "appid", value: apiKey)
+        ]
+       
+        guard let url = urlComponents.url else { return }
+        
+        let task = URLSession.shared.dataTask(with: url) { (data, response, error) in
+           
+            guard let data = data else { return }
+            
+            do {
+                let decodedJSON = try JSONDecoder().decode(WeatherCodableStruct.self, from: data)
+                
+
+            } catch {
+                print("Unable to fetch JSON Data...")
+            }
+        }
+        task.resume()
+    }
     
     // MARK:- Helper Methods
     private func registerTabelViewCell() {
         tableView.register(WeatherCell.self, forCellReuseIdentifier: weatherCellID)
+    }
+    
+    private func updateUserLocation() {
+        
+        locationManager.delegate = self
+        locationManager.requestWhenInUseAuthorization()
+        locationManager.startUpdatingLocation()
+    }
+    
+    private func convertKelvinIntoCelsius(temp kelvin: Double) -> Int {
+        Int(kelvin - 273.15)
     }
 }
 
@@ -48,10 +93,28 @@ extension WeatherVC: CLLocationManagerDelegate {
     
     func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
         
+        let alertController = UIAlertController(title: "Alert", message: "Location Unavailable", preferredStyle: .alert)
+        let action          = UIAlertAction(title: "OK", style: .default, handler: nil)
+        
+        alertController.addAction(action)
+        present(alertController, animated: true)
+        
     }
     
     func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
         
+        let location = locations[locations.count - 1]
+        
+        // Negative Value means invalid Coordinates
+        if location.horizontalAccuracy > 0 {
+            
+            locationManager.stopUpdatingLocation()
+            locationManager.delegate = nil
+            
+            let latitude    = String(location.coordinate.latitude)
+            let longitude   = String(location.coordinate.longitude)
+            
+            fetchWeatherData(with: hostURL, latitude: latitude, longitude: longitude, apiKey: apiKey)
+        }
     }
-    
 }
---
+++ ConvoAssignWeatherApp/Model/WeatherCodableStruct.swift
@@ -0,0 +1,99 @@
+//
+//  WeatherCodableStruct.swift
+//  ConvoAssignWeatherApp
+//
+//  Created by Umer Khan on 23/05/2020.
+//  Copyright © 2020 Umer Khan. All rights reserved.
+//
+
+import UIKit
+
+// MARK:- MainStruct
+struct WeatherCodableStruct: Decodable {
+    
+    let cod:        String
+    let message:    Int
+    let cnt:        Int
+    let list:       [List]?
+    let city:       City?
+}
+
+// MARK:- List
+struct List: Decodable {
+    
+    let dt:         Int
+    let main:       Main?
+    let weather:    [Weather]?
+    let clouds:     Clouds?
+    let wind:       Wind?
+    let rain:       Rain?
+    let sys:        Sys?
+    let dtTxt:      String
+
+    enum CodingKeys: String, CodingKey {
+        case dt, main, weather, clouds, wind, rain, sys
+        case dtTxt = "dt_txt"
+    }
+}
+
+// MARK:- City
+struct City: Codable {
+    
+    let id: Int
+    let name, country:  String
+    let coord:  Coord?
+    let timezone, sunrise, sunset:   Int
+}
+
+// MARK:- Main
+struct Main: Codable {
+    
+    let temp, feelsLike, tempMin, tempMax, tempKf:  Double
+    let pressure, seaLevel, grandLevel, humidity:   Int
+
+    enum CodingKeys: String, CodingKey {
+
+        case temp       = "temp"
+        case feelsLike  = "feels_like"
+        case tempMin    = "temp_min"
+        case tempMax    = "temp_max"
+        case pressure   = "pressure"
+        case seaLevel   = "sea_level"
+        case grandLevel = "grnd_level"
+        case humidity   = "humidity"
+        case tempKf     = "temp_kf"
+    }
+}
+
+// MARK:- Weather
+struct Weather: Codable {
+    
+    let id:             Int
+    let main:           String
+    let description:    String
+    let icon:           String
+}
+
+// MARK:- Clouds
+struct Clouds: Codable { let all: Int }
+
+// MARK:- Wind
+struct Wind: Codable {
+    let speed:  Double
+    let deg:    Int
+}
+
+// MARK:- Rain
+struct Rain: Codable {
+    let h3: Double
+
+    enum CodingKeys: String, CodingKey {
+        case h3 = "3h"
+    }
+}
+
+// MARK:- Sys
+struct Sys: Codable { let pod: String }
+
+// MARK:- Coord
+struct Coord: Codable { let lat, lon: Double }
